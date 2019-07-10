c857ee9d96ee6a7b8a50fa52dffb8a6a8707bf7a
顾杰义 <18535@etransfar.com>
2019-07-10 10:04:06 +0800

    save

--- typescript-react/template/src/App.tsx
+++ typescript-react/template/src/App.tsx
@@ -1,15 +1,19 @@
 import "./assets/main.less";
 import * as React from "react";
 import { HashRouter as Router, Route, Link } from "react-router-dom";
-import {Provider} from "react-redux";
-import Home from './components/Home';
-import About from './components/About';
-import HomeWarp from './containers/AddCount';
 
-import rootReducer from "./reducer/index";
+import About from './components/About';
+
+
+{{#if redux}}
+import {Provider} from "react-redux";
 import {createStore} from "redux";
 const store = createStore(rootReducer);
-
+import rootReducer from "./reducer/index";
+import Home from './containers/AddCount';
+{{else}}
+import Home from './components/Home';
+{{/if}}
 class App extends React.Component{
 
   componentDidMount(){
@@ -21,7 +25,7 @@ class App extends React.Component{
         <Router >
           <h1 style={{margin:"1rem auto","textAlign": "center"}}>hello world</h1>
           <img src={require("./assets/react-icon.png")} style={{"display":"block",margin:"0.5rem auto",width:"2rem"}} />
-          <Route path="/home" component={HomeWarp}/>
+          <Route path="/home" component={Home}/>
           <Route path="/about" component={About}/>
           <div style={{"textAlign":"center"}}>
             <Link to="/home" style={{margin:"0.5rem"}}>home</Link>
@@ -33,8 +37,10 @@ class App extends React.Component{
   }
 }
 
+
+{{#if redux}}
 const Providers  = ()=>(<Provider store={store}><App/></Provider>)
-
-
-
 export default Providers;
+{{else}}
+export default App;
+{{/if}}
